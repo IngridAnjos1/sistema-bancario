028b9240624060c28033740a63fcff3e40ca181d
Ingrid Anjos <ingrid_anjos@hotmail.com>
2024-03-21 14:27:59 -0300

    feat: criacao do metodo getMapping no AccountController

--- src/main/java/ingrid/anjos/sistemabancario/controller/AccountController.java
+++ src/main/java/ingrid/anjos/sistemabancario/controller/AccountController.java
@@ -2,26 +2,38 @@ package ingrid.anjos.sistemabancario.controller;
 
 import ingrid.anjos.sistemabancario.model.Account;
 import ingrid.anjos.sistemabancario.model.AccountDTO;
+import ingrid.anjos.sistemabancario.service.AccountService;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
+  private final AccountService service;
+
+  public AccountController(AccountService service) {
+    this.service = service;
+  }
 
   @PostMapping
   public ResponseEntity<Account> create(@RequestBody AccountDTO accountDTO) {
-    return
+    Account newAccount = this.service.createAccount(accountDTO);
+    return ResponseEntity.ok().body(newAccount);
   }
-};
 
+  @GetMapping
+  public ResponseEntity<List<Account>> getAll() {
+    List<Account> categories = this.service.getAll();
+    return ResponseEntity.ok().body(categories);
+  }
 
-
-
-//Um controlador é responsável por lidar com as solicitações HTTP,
-// geralmente vindo de um cliente da web, e retornar uma resposta apropriada.
-// Os métodos dentro de uma classe anotada com @Controller geralmente são mapeados para endpoints HTTP específicos,
-// como GET, POST, PUT ou DELETE.//
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<Account>> findById(@PathVariable("id") String id) {
+    Optional<Account> newAccount = this.service.getById(id);
+    return ResponseEntity.ok().body(newAccount);
+  }
+}
