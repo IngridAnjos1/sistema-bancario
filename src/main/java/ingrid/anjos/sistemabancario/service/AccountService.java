e9e1606e06eedd906bce8c009a24bed36ee8e018
Ingrid Anjos <ingrid_anjos@hotmail.com>
2024-03-21 14:26:00 -0300

    feat: criacao do accountService

--- src/main/java/ingrid/anjos/sistemabancario/service/AccountService.java
+++ src/main/java/ingrid/anjos/sistemabancario/service/AccountService.java
@@ -1,7 +1,42 @@
 package ingrid.anjos.sistemabancario.service;
 
+import ingrid.anjos.sistemabancario.model.Account;
+import ingrid.anjos.sistemabancario.model.AccountDTO;
+
+import ingrid.anjos.sistemabancario.repositories.AccountRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountService {
+  private final AccountRepository repository;
+
+
+  public AccountService(AccountRepository repository) {
+    this.repository = repository;
+  }
+
+  public Account createAccount(AccountDTO accountDTO) {
+    Account newAccount = new Account(accountDTO);
+    newAccount.setAccountNumber(accountNumber());
+    this.repository.save(newAccount);
+    return newAccount;
+  }
+
+  public List<Account> getAll() {
+    return this.repository.findAll();
+  }
+
+  public Optional<Account> getById(String id) {
+    return this.repository.findById(id);
+  }
+
+  private Long accountNumber() {
+    var intSize = this.repository.findAll().size();
+    return (long) intSize + 1;
+  }
 }
+
+
